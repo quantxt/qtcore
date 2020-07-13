bed02d58f7a4ff77a25630c259311cf418230f5c
Matin Kamali <matin_kamali@yahoo.com>
2020-07-13 13:07:02 -0400

    simplify QtDocument and helper bases classes

--- src/main/java/com/quantxt/doc/QTDocument.java
+++ src/main/java/com/quantxt/doc/QTDocument.java
@@ -7,7 +7,7 @@ import com.quantxt.types.ExtIntervalSimple;
 
 import com.quantxt.types.ExtInterval;
 
-public abstract class QTDocument {
+public class QTDocument {
 
     public enum Language {
         ENGLISH, SPANISH, GERMAN, FRENCH, ARABIC, RUSSIAN, FARSI, JAPANESE, PORTUGUESE
@@ -48,13 +48,10 @@ public abstract class QTDocument {
         this.helper = helper;
     }
 
-    // Getters
-
-    public abstract List<QTDocument> getChunks(CHUNK chunk);
-
-    public abstract String Translate(String text, Language inLang, Language outLang);
-
-    public abstract boolean isStatement(String s);
+    // implement this
+    public List<QTDocument> getChunks(CHUNK chunk){
+        return null;
+    }
 
     public void sortValues(){
         if (this.values == null) return;
--- src/main/java/com/quantxt/doc/QTDocumentHelper.java
+++ src/main/java/com/quantxt/doc/QTDocumentHelper.java
@@ -3,6 +3,7 @@ package com.quantxt.doc;
 import java.util.List;
 import java.util.Set;
 
+import com.quantxt.types.DictSearch;
 import com.quantxt.types.ExtIntervalSimple;
 
 public interface QTDocumentHelper {
@@ -17,5 +18,13 @@ public interface QTDocumentHelper {
 
     Set<String> getStopwords();
 
+    String extractHtmlExcerptForDocument(QTDocument qtDocument);
+
     String getValues(String orig, String context, List<ExtIntervalSimple> list);
+
+    void extract(QTDocument qtDocument,
+                 List<DictSearch> extractDictionaries,
+                 boolean canSearchVertical,
+                 String context);
+
 }
