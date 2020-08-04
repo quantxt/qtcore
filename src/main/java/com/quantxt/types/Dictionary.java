d8ffbd7079e7ca97852001dd46f1735bd9d958d5
Matin Kamali <matin_kamali@yahoo.com>
2020-08-03 21:56:59 -0400

    removed default dictionary paddings

--- src/main/java/com/quantxt/types/Dictionary.java
+++ src/main/java/com/quantxt/types/Dictionary.java
@@ -17,9 +17,6 @@ public  class Dictionary implements Serializable {
 
     public enum ExtractionType {NUMBER, DATETIME, REGEX };
 
-    final private static Pattern default_skip_between_value = Pattern.compile("^\\s+$");
-    final private static Pattern default_skip_between_key_and_value = Pattern.compile("^[\\s\"'\\/\\\\\\.\\(\\)\\-\\_\\&\\%\\~\\?\\@\\$\\:;%,]+$");
-
     final private List<DictItm> vocab; // category to str s
     final protected String name;
     final protected String id;
@@ -36,10 +33,6 @@ public  class Dictionary implements Serializable {
         this.vocab = vocab;
         this.name = name;
         this.id = id == null ? UUID.randomUUID().toString() : id;
-        this.skip_between_values = default_skip_between_value;
-        this.skip_between_key_and_value = default_skip_between_key_and_value;
-        this.groups = null;
-        this.pattern = null;
     }
 
     public Dictionary(List<DictItm> vocab,
@@ -52,14 +45,12 @@ public  class Dictionary implements Serializable {
                       int [] groups) {
         this.vocab = vocab;
         this.valType = valType;
-        this.skip_between_values = skip_between_values != null ? skip_between_values :
-                default_skip_between_value;
+        this.skip_between_values = skip_between_values;
         this.groups = groups;
         this.id = id == null ? UUID.randomUUID().toString() : id;
         this.name = name;
         this.pattern = pattern;
-        this.skip_between_key_and_value = skip_between_key_and_value != null ? skip_between_key_and_value
-                :default_skip_between_key_and_value;
+        this.skip_between_key_and_value =  skip_between_key_and_value;
     }
 
     public List<DictItm> getVocab() {
